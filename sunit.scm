8c259257e0358046beacf33d69d967097c5afa6b
anton164 <anton_abilov@hotmail.com>
2014-01-30 23:04:13 +0100

    unit-test returns test result string instead of display

--- sunit.scm
+++ sunit.scm
@@ -7,19 +7,33 @@
 
 (define (unit-test name predicate expected try)
   (if (predicate expected try)
-      (display (string-append "Test '" name "' passed\n"))
-      (display (string-append "Test '" name "' failed! " (tostring expected) 
-                              " not equal to " (tostring try) "\n"))))
+      (string-append "Test '" name "' passed")
+      (string-append "Test '" name "' failed! " (tostring expected) 
+                              " not equal to " (tostring try) "")))
+
+
+(define (unit-tests . tests)
+  (if (null? tests)
+      "Tests passed" 
+   (apply unit-tests (cdr tests))))
 
 #! GOAL: Allow the following:
-;; (unit-tests
-;;   ("Test 1" = 2 2)
-;;   ("Test 2" < 3 4))
+(unit-tests
+   (list "Test 1" = 2 2)
+   (list "Test 2" < 3 4))
 
-(define (unit-tests tests)
-  (apply unit-test tests))
+#| Preferrable output:
+Test run successful.
+x out of y tests passed.
+------------------------
+Test run failed.
+x out of y tests passed.
+The following tests failed:
+...
+...
+|#
 
 (define tests
-  '(("fact2=2" = 2 (fact 2))
-    ("fact3=6" = 6 (fact 3))))
+  (list (list "fact2=2" = 2 (* 2 1))
+    (list "fact3=6" = 6 (* 3 2 1))))
 
