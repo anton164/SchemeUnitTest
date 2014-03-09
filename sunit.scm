bc7fe3d66ca245d1e30dbd584848755890265567
anton164 <anton_abilov@hotmail.com>
2014-03-09 19:15:05 +0100

    Improved test block output

--- sunit.scm
+++ sunit.scm
@@ -1,5 +1,24 @@
 ;; SchemeUnitTest
-;; Built-in list->string only takes a list of characters
+
+;; Helper methods
+(define (all? pred lst)
+  (if (null? lst)
+      #t
+      (and (pred (car lst)) (all? pred (cdr lst)))))
+
+(define (reduce f lst)
+  (if (null? lst)
+      0
+      (f (car lst) (reduce f (cdr lst)))))
+
+(define (substring? str substr)
+  (define (string-has-char? str char)
+    (member char (string->list str)))
+    (if (or (= (string-length substr) 0) (equal? str substr))
+          #t
+        (if (= (string-length str) 0)
+          #f
+          (all? (lambda (char) (string-has-char? str char)) (string->list substr)))))
 
 (define (tostring exp)
   (define (list-->string lst)
@@ -19,7 +38,7 @@
 
 (define (unit-test name predicate expected try)
   (if (predicate expected try)
-      (string-append "Test '" name "' passed")
+      (string-append "Test '" name "' passed.")
       (string-append "Test '" name "' failed! Expected: " (tostring expected) 
                               " Returned: " (tostring try) "")))
 
@@ -32,14 +51,30 @@
         (begin
           (display (string-append (car test-results) "\n"))
           (output (cdr test-results)))))
-  (define (run-tests tests t-success t-fail)
+  (define (run-tests tests)
     (map (lambda (x) (apply unit-test x)) tests))
+  (define (test-result->succfail results)
+    ;; Convert test results to a pair of numbers representing successful and failed tests.
+    (cons 
+     (reduce + (map (lambda (result) (if (substring? result " passed.") 1 0)) results)) ;; successful tests
+     (reduce + (map (lambda (result) (if (substring? result " failed!") 1 0)) results)))) ;; failed tests
+  (define (final-result succfail)
+    (let* ((success (car succfail))
+           (fails (cdr succfail))
+           (num-tests (+ success fails)))
+      (if (> fails 0)
+          (string-append "Test block failed.\n"
+                         (number->string success) " out of " (number->string num-tests) " tests passed.")
+          (string-append "Test block successful.\n"
+                         (number->string success) " out of " (number->string num-tests) " tests passed."))))
   (begin
+    (let* ((test-result (run-tests tests))
+      (succfail (test-result->succfail test-result)))
     (display (string-append "Running test-block: " name "\n"))
     (display "---------------------------\n")
-    (output (run-tests tests 0 0))
+    (output test-result)
     (display "---------------------------\n")
-    (display "Test block complete.\n")))
+    (display (string-append (final-result succfail) "\n\n")))))
 
 #| Preferrable output:
 Test run successful.
